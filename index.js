65463ce5b1bef2a016a666821d41a6f4cc7faac8
Kamatoro64 <chester91.jp@gmail.com>
2020-10-28 15:14:57 +0800

    + logging

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ client.once('ready', async () => {
 	//client.guilds Type is GuildManager | client.guilds.cache Type is Collection<Snowflake, Guild> 
 	const currentGuild = client.guilds.cache.get(serverId); // Returns Guild Object (Represents a guild/server)
 
+	console.log(currentGuild.members.cache)
 
 
 	await currentGuild.members.fetch().then(
